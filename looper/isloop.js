76d17dea8122f2aa1d250c727069572211f19241
Bailey McKelway <bjlmckelway@gmail.com>
2020-04-08 17:21:46 -0400

    FINAL

--- looper/isloop.js
+++ looper/isloop.js
@@ -3,22 +3,27 @@
 //Complete this algo
 
 const isLoop = (linkedlist, arr = []) => {
-    /// create an array to store values of the node.
-    /// check value of the node, calling by node.value.
-    /// check if array includes node's value
-    // console.log(linkedlist.head.next)
-    let Node = linkedlist
-    console.log(Node)
-    if (arr.includes(Node.value)){
-     return false;
-    } else if (!Node.next && !arr.includes(Node.value)){
-        return true;
-    } else {
-        arr.push(Node.value);
-        return isLoop(Node.next);
-    }
-};
+  /// create an array to store values of the node.
+  /// check value of the node, calling by node.value.
+  /// check if array includes node's value
+  // console.log(linkedlist.head.next)
+  let Node;
+  if (linkedlist.head) {
+    Node = linkedlist.head;
+  } else {
+    Node = linkedlist;
+  }
 
+  if (arr.includes(Node.value)) {
+    return false;
+  } else if (!Node.next && !arr.includes(Node.value)) {
+    return true;
+  } else {
+    arr.push(Node.value);
+
+    return isLoop(Node.next);
+  }
+};
 
 /*
 EXTRA CREDIT:
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,21 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr, counter = 1) => {
+  if (arr[0] >= arr.length - 1) {
+    return counter;
+  }
 
+  let currentPostionValue = arr[0];
+
+  // temp array = startArray.slice(currentPosition)
+  let tempArray = arr.slice(1, currentPostionValue + 1);
+
+  // find max value
+  let maxValue = Math.max(...tempArray);
+  counter = counter + 1;
+
+  return minJumps(arr.slice(tempArray.indexOf(maxValue) + 1), counter);
 };
 
-module.exports = minJumps
+module.exports = minJumps;
